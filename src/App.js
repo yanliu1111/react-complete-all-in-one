86d61a400a14d6a1ef3e22167cb5d63ad6a44f07
yanliu1111 <liuyan0504@gmail.com>
2025-07-11 18:06:33 -0600

    feat: json data

--- src/App.js
+++ src/App.js
@@ -12,9 +12,36 @@ import PostPage from './PostPage';
 import React from 'react';
 
 const App = () => {
+  const [posts, setPosts] = useState([
+    {
+      "id": 1,
+      "title": "My First Post",
+      "datetime": "July 01, 2021 11:17:36 AM",
+      "body": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      "id": 2,
+      "title": "My 2nd Post",
+      "datetime": "July 01, 2021 11:17:36 AM",
+      "body": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      "id": 3,
+      "title": "My 3rd Post edit",
+      "datetime": "January 07, 2023 1:15:46 PM",
+      "body": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa! edit"
+    },
+    {
+      "id": 4,
+      "title": "My Fourth Post",
+      "datetime": "July 01, 2021 11:17:36 AM",
+      "body": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    }
+  ]);
+  const [search, setSearch] = useState('');
   return (
     <div className='App'>
-      <Header />
+      <Header title = 'React JS Blog' />
       <Nav />
       <Routes>
         {/* <Route path="/" element={<Home />} />
