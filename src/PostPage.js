6ab19f2196f789e9d541ebe85e53225cc8914614
yanliu1111 <liuyan0504@gmail.com>
2025-07-13 13:29:39 -0600

    feat: postPage done

--- src/PostPage.js
+++ src/PostPage.js
@@ -1,9 +1,33 @@
+import { Link, useParams } from 'react-router-dom'
+
 import React from 'react'
 
-const PostPage = () => {
+const PostPage = ({ posts, handleDelete }) => {
+    const { id } = useParams();
+    const post = posts.find(post => (post.id).toString() === id);
   return (
-    <main className='postPage'>
-      <h1>PostPage</h1>
+    <main className='PostPage'>
+        <article className='post'>
+            {post && 
+                <>
+                    <h2>{post.title}</h2>
+                    <p className='postDate'>{post.datetime}</p>
+                    <p className='postBody'>{post.body}</p>
+                    <Link to={`/edit/${post.id}`}><button className='editButton'>Edit Post</button></Link>
+                    <button className='deleteButton' onClick={() => handleDelete(post.id)}>
+                        Delete Post
+                    </button>
+                </>
+            }
+            {!post &&
+                <>
+                    <h2>Post Not Found</h2>
+                    <p>
+                        <Link to="/">Visit our Homepage</Link>
+                    </p>
+                </>
+            }
+        </article>
     </main>
   )
 }
