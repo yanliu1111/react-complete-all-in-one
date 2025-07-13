4fe5f9d3516a80a516ad65987be558a7f0954214
yanliu1111 <liuyan0504@gmail.com>
2025-07-12 21:19:48 -0600

    feat:missing page

--- src/Missing.js
+++ src/Missing.js
@@ -1,11 +1,15 @@
+import { Link } from 'react-router-dom'
 import React from 'react'
 
 const Missing = () => {
-  return (
-    <header className='missing'>
-      <h1>Missing</h1>
-    </header>
-  )
+    return (
+        <main className='Missing'>
+            <h2>Page Not Found</h2>
+            <p>
+                <Link to="/">Visit our Homepage</Link>
+            </p>
+        </main>
+    )
 }
 
 export default Missing
