b65e93cc49922bd1c54491a4dc66763ce65f609a
yanliu1111 <liuyan0504@gmail.com>
2025-07-15 23:23:00 -0600

    feat: index done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import './index.css';
 
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+        <Routes>
+            <Route path="/*" element={<App/>} />
+        </Routes>
     </Router>
   </React.StrictMode>,
 );
