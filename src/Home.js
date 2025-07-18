274499c9c8d3ad28001b2bfdb1037f9df70dcaf5
yanliu1111 <liuyan0504@gmail.com>
2025-07-17 18:34:47 -0600

    style: update

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Home = ({posts}) => {
   return (
-    <main className='home'>
+    <main className='Home'>
       {posts.length ? (
         <Feed posts={posts} />
       ) : ( <p style={{marginTop:"2em"}}>No posts to display</p>
